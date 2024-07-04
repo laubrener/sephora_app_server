05fc3b81b058375892e2ed1d5735930b9396076f
laubrener <79546818+laubrener@users.noreply.github.com>
2024-07-04 12:49:16 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+const app = express();
+
+const publicPath = path.resolve(__dirname, "public");
+
+app.use(express.static(publicPath));
+
+
+app.listen(process.env.PORT, ((err) => {
+    if(err) throw new Error(err);
+
+    console.log('Servidor corriendo en puerto', process.env.PORT);
+}));
